455cb06651b5c0531321234d4943c7dc7fc67ce1
bihongbin3027 <410362365@qq.com>
2018-03-23 18:29:49 +0800

    添加移动端事件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import FastClick from 'fastclick'
 import registerServiceWorker from './registerServiceWorker'
+import App from './App'
 import './style/base.css'
+
+FastClick.attach(document.body)
 
 const render = Comment => {
   ReactDOM.render(
